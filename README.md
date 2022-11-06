8430bbf11af753299fa13ac7809d99db99daad5c
Umut Savas <daniel230994@gmail.com>
2022-11-06 13:11:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Valorant Background Changer
+A powershell script to change your Valorant background easily.
+
+## How to use it
+Download the **bgChanger** script and run it with "right-click -> Run with PowerShell".
+
+## Limitation
+This script only works on Windows machines. It won't work on macOS or any Linux OS.
+
+## Contributing
+If you have a suggestion that would make this better, please fork the repo and create a pull request.
+1. Fork the Project
+2. Create your Feature Branch (git checkout -b feature/AmazingFeature)
+3. Commit your Changes (git commit -m 'Add some AmazingFeature')
+4. Push to the Branch (git push origin feature/AmazingFeature)
+5. Open a Pull Request
---
+++ bgChanger.ps1
@@ -0,0 +1,59 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+function changeBG($mp4File, $clientPath) {
+    try
+    {
+        Copy-Item $mp4File -Destination "$clientPath\HomeScreen.mp4" -Force
+        Copy-Item $mp4File -Destination "$clientPath\Harbor_HomeScreen.mp4" -Force
+        [System.Windows.Forms.MessageBox]::Show("Valorant background has been set successfully.", "Info", 0, [System.Windows.Forms.MessageBoxIcon]::Information)
+        exit;
+    }
+    catch
+    {
+        [System.Windows.Forms.MessageBox]::Show("Couldn't copy file to valorant directory from: $mp4File, to: $clientPath", "Error",
+                0,
+                [System.Windows.Forms.MessageBoxIcon]::Error
+        )
+    }
+}
+
+#Check if client is running
+$clientPath = Get-Process | Select-Object -ExpandProperty Path | select-string "RiotClientServices"
+
+if (!$clientPath)
+{
+    [System.Windows.Forms.MessageBox]::Show("Riot Games client is not running.", "Error", 0, [System.Windows.Forms.MessageBoxIcon]::Error)
+    exit;
+}
+
+#Select video and check for mp4 format
+[System.Windows.Forms.MessageBox]::Show(
+        "Please select the video in mp4 format for your Valorant background", "Info",
+        0,
+        [System.Windows.Forms.MessageBoxIcon]::Information
+)
+
+$fileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') }
+$fileBrowser.ShowDialog()
+$mp4File = $fileBrowser.FileName
+
+if ((Get-Item $mp4File).Extension -ne ".mp4")
+{
+    [System.Windows.Forms.MessageBox]::Show("File is not in mp4 format", "Error", 0, [System.Windows.Forms.MessageBoxIcon]::Error)
+    exit;
+}
+
+#Set correct destination path
+$clientPath = (Get-Item $clientPath).DirectoryName
+$clientPath = "$clientPath\..\VALORANT\live\ShooterGame\Content\Movies\Menu"
+changeBG $mp4File $clientPath
+
+#In case copying failed earlier, select installation path and try to copy again
+$foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Select the VALORANT folder of your game is installed."
+    $foldername.rootfolder = "MyComputer"
+$foldername.ShowDialog()
+$clientPath = $foldername.SelectedPath
+$clientPath = "$clientPath\live\ShooterGame\Content\Movies\Menu"
+
+changeBG $mp4File $clientPath
